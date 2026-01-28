17f32f53596c9d6f1c761f7fbd10cd7cf362b395
CaioSpilotro <caiospilotro@gmail.com>
2026-01-28 16:05:25 -0300

    feat: New Challenge solved

---
+++ Challenges/Terraform/Create-Iam-Policy/main.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_policy" "new_policy" {
+    name    =   "iampolicy_kirsty"
+    policy  =   jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = [
+                    "ec2:Describe*",
+                ],
+                Effect = "Allow"
+                Resource = "*"
+            }
+        ]
+    })
+}
+
+# Documentation Reference: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+# Command Validation: aws iam list-policies --query 'Policies[?starts_with(PolicyName,`iampolicy_kirst`)]'
